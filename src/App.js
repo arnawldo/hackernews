1dec59e076da226fb0751bdd8022a67ec7fc64ed
Arnold Taremwa <taremwaarnold@gmail.com>
2018-07-02 18:40:12 +0300

    API working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
+
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
+
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
 
 const list = [
     {
@@ -28,19 +39,26 @@ const Button = ({ onClick, className='', children }) =>
 const isSearched = searchTerm => item =>
     item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
-const Search = ({ value, onChange, children }) =>
-    <form>
-        { children }
+const Search = ({
+                    value,
+                    onChange,
+                    onSubmit,
+                    children
+}) =>
+    <form onSubmit={onSubmit}>
         <input
             type="text"
             value={value}
             onChange={onChange}
         />
+        <button type="submit">
+            { children }
+        </button>
     </form>;
 
-const Table = ({list, pattern, onDismiss}) =>
+const Table = ({list, onDismiss}) =>
     <div className="table">
-        {list.filter(isSearched(pattern)).map(item =>
+        {list.map(item =>
             <div key={item.objectID} className="table-row">
                 <span style={{ width: '40%' }}>
                     <a href={item.url}>{item.title}</a>
@@ -67,41 +85,143 @@ const Table = ({list, pattern, onDismiss}) =>
     </div>;
 
 class App extends Component {
+    _isMounted = false;
 
     constructor(props) {
         super(props);
 
         this.state = {
-            list,
-            searchTerm: '',
+            results: null,
+            searchKey: '',
+            searchTerm: DEFAULT_QUERY,
+            error: null,
         };
 
+        this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this);
+        this.setSearchTopStories = this.setSearchTopStories.bind(this);
+        this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this);
         this.onDismiss = this.onDismiss.bind(this);
         this.onSearchChange = this.onSearchChange.bind(this);
+        this.onSearchSubmit = this.onSearchSubmit.bind(this);
+    }
+
+    needsToSearchTopStories(searchTerm) {
+        return !this.state.results[searchTerm];
+    }
+
+    setSearchTopStories(result) {
+        const { hits, page } = result;
+        const { results, searchKey } = this.state;
+
+        const oldHits = results && results[searchKey]
+            ? results[searchKey].hits
+            : [];
+        const updatedHits = [
+            ...oldHits,
+            ...hits
+        ];
+        this.setState({
+            results: {
+                ...results,
+                [searchKey]: { hits: updatedHits, page }
+            }
+        });
+    }
+
+    fetchSearchTopStories(searchTerm, page=0) {
+        axios(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+            .then(result => this._isMounted && this.setSearchTopStories(result.data))
+            .catch(error => this._isMounted && this.setState({ error }));
+    }
+
+    componentDidMount() {
+        this._isMounted = true;
+
+        const { searchTerm } = this.state;
+        this.setState({ searchKey: searchTerm });
+        this.fetchSearchTopStories(searchTerm);
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
     }
 
     onDismiss(id) {
+        const { searchKey, results } = this.state;
+        const { hits, page } = results[searchKey];
+
         const isNotId = item => item.objectID !== id;
-        const updatedList = this.state.list.filter(isNotId);
-        this.setState({ list: updatedList });
+        const updatedHits = hits.filter(isNotId);
+        this.setState({
+            results: {
+                ...results,
+                [searchKey]: { hits: updatedHits, page }
+            }
+        });
     }
 
     onSearchChange(event) {
         this.setState({ searchTerm: event.target.value })
     }
 
+    onSearchSubmit(event) {
+        const { searchTerm } = this.state;
+        this.setState({ searchKey: searchTerm });
+        if (this.needsToSearchTopStories(searchTerm)) {
+            this.fetchSearchTopStories(searchTerm);
+        }
+        event.preventDefault();
+    }
+
     render() {
-        const { list, searchTerm } = this.state;
+        const {
+            results,
+            searchTerm,
+            searchKey,
+            error
+        } = this.state;
+
+        const page = (
+            results &&
+            results[searchKey] &&
+            results[searchKey].page
+        ) || 0;
+
+        const list = (
+            results &&
+            results[searchKey] &&
+            results[searchKey].hits
+        ) || [];
 
         return (
         <div className="App">
             <div className="page">
                 <div className="interactions">
-                    <Search value={searchTerm} onChange={this.onSearchChange}>
+                    <Search
+                        value={searchTerm}
+                        onChange={this.onSearchChange}
+                        onSubmit={this.onSearchSubmit}
+                    >
                         Search
                     </Search>
                 </div>
-                <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss}/>
+                { error
+                    ? <div className="interactions">
+                        <p>Something wrong happened</p>
+                    </div>
+                    : <Table
+                        list={list}
+                        onDismiss={this.onDismiss}
+                    />
+                }
+
+                <div className="interactions">
+                    <Button
+                        onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}
+                    >
+                        More
+                    </Button>
+                </div>
             </div>
         </div>
         );
